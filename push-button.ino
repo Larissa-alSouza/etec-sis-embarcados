44c9ccfe32191ad9144d7bb16b89a02543023454
Larissa Alves de Souza <162065841+Larissa-alSouza@users.noreply.github.com>
2025-08-28 19:39:00 -0300

    Create push-button.ino

---
+++ push-button.ino
@@ -0,0 +1,23 @@
+int ledPin = 2;
+int inPin = 3;
+int val = 0;
+
+void setup()
+{
+  pinMode(ledPin, OUTPUT);
+  pinMode(inPin, INPUT);
+}
+
+void loop()
+{
+  val = digitalRead(inPin);
+  
+  if(val == HIGH)
+  {
+    digitalWrite(ledPin, HIGH);
+  }
+  else
+  {
+    digitalWrite(ledPin, LOW);
+  }
+}
